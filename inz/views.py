9946be5cb6ecc82f3aff42f139daf066d0f00e60
pawelolszewski <pawel.colo@gmail.com>
2020-02-19 20:33:16 +0100

    dodanie index html

--- inz/views.py
+++ inz/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-
+#from django.temeplate import loader
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Elo mordo!")
+   #temeplate=loader.get_temeplate("index.html")
+    return render(request, "inz/index.html")
