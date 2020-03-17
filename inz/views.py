5140f831023b4e6e5660106a5d0e4d51806eb580
pawelolszewski <pawel.colo@gmail.com>
2020-03-17 12:56:55 +0100

    dodanie view py

--- inz/views.py
+++ inz/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 def index(request):
    #temeplate=loader.get_temeplate("index.html")
     return render(request, "inz/index.html")
+    return render(request, "inz/kontakt.html")
+    return render(request, "inz/Menu.html")
+    return render(request, "inz/O_nas.html")
+    return render(request, "inz/Pokoje.html")
+    return render(request, "inz/warianty_wyzywienia.html")
